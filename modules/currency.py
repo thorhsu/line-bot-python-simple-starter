968f34b94e59b46dbd4f703166ccb786710d36b0
andy19910102 <andy19910102@gmail.com>
2019-04-17 17:21:10 +0800

    Project init

---
+++ modules/currency.py
@@ -0,0 +1,15 @@
+
+# 貨幣
+
+
+class Currency:
+    # 定義貨幣屬性: 名稱name、銀行買入價bid、銀行賣出價offer
+    pass
+
+# 貨幣爬蟲
+
+
+class CurrencyCrawler:
+    # 貨幣爬蟲屬性: 目標網址url(https://rate.bot.com.tw/xrt?Lang=zh-TW)、爬取的資料data
+    # 貨幣爬蟲函數: 爬取資料fetch_data、取得指定貨幣資訊get_data
+    pass
